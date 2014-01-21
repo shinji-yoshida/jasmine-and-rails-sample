5a614e1638b34ed4b33c70aed1b312b535a5fdf8
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2014-01-21 16:54:47 +0900

    create rails project

---
+++ fizz_buzz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FizzBuzz::Application.config.secret_token = '812354a746aeba38e39529fe17e4529fe3ad3e2f5a58d9588d2a2edeb2593cf83f576b3a81b4c359d08986ad694b8ba37032b350828bfcf63d8de7d1fe26133d'
